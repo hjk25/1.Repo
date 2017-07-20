277ea00d9a015dc37322366218c0a8de6bc1015a
hjk25 <hyunjin.kim@nbcuni.com>
2017-07-20 11:39:12 -0400

    Add files via upload
    R-Week1 HW

---
+++ R-Week1 HW-HJK.R
@@ -0,0 +1,21 @@
+# 1. Write a loop that calculates 12 factorial
+y <- 1
+for(a in 1:12)
+{
+  y <- y * a
+}
+y
+
+
+# 2. Show how to create a numeric vector that contains the sequence from 20 to 50 by 5
+d <- 4:10
+d
+Seq <- 5 * d
+as.numeric(Seq)
+
+
+#3.Show how to take a trio of input numbers a, b, and c and implement the quadratic equation
+ax^2 + b*x + c = 0
+#function(one) <- (-b+root(b^2-4*a*c))/(2*a)
+#function(two) <- (-b-root(b^2-4*a*c))/(2*a)
+
